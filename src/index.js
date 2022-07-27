a0440250d65ed66888820da4c14b5723d05be16e
Your Name <you@example.com>
2022-07-27 03:36:55 -0500

    improving messages

--- src/index.js
+++ src/index.js
@@ -9,12 +9,22 @@ boton.addEventListener("click", function(){
     //obtener lo que escribió la usuaria
     const numberCard = number.value;
     //mostrar los números enmascarados en el input
-    document.getElementById("CardNumber").value = validator.maskify(numberCard);
-    let isValid = validator.isValid(numberCard);
-    console.log(isValid)
-    if (isValid >=true){
-        alert("Tarjeta válida, continúa con el paso 4")
-    } else {
-        alert ("Prueba con otra tarjeta")
+    console.log(numberCard);
+    let errors = document.getElementById("alerts");
+    if(numberCard==''){
+        errors.className ="messages-error";
+        errors.innerHTML="Debe introducir un número";
+    }else{
+        document.getElementById("CardNumber").value = validator.maskify(numberCard);
+    
+        let isValid = validator.isValid(numberCard);
+        if (isValid >=true){
+            errors.className ="messages-ok";
+            errors.innerHTML="Tarjeta válida, continúa con el paso 4";
+        } else {
+            errors.className ="messages-error";
+            errors.innerHTML="Prueba con otra tarjeta";
+        }
     }
+    
 });
